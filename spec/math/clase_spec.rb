11e2962bbb9d89925db2fcc8bc8c568b98f37379
KevinRobayna <k3vinaso@gmail.com>
2013-10-25 14:47:41 +0100

    Prueba de clase

---
+++ spec/math/clase_spec.rb
@@ -0,0 +1,12 @@
+require "fraction.rb"
+
+describe Fraction do
+    before :all do
+        A = Fraction.new(1,1)           
+        B = Fraction.new(1,4)
+    end 
+
+    describe "Test de clase: |f1 * f2| Multiplicacion y ABS" do
+    
+    end
+end
