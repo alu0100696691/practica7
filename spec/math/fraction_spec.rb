9fcc023e307f2a338a07af092afc72e4325aea27
KevinRobayna <k3vinaso@gmail.com>
2013-10-25 10:01:14 +0100

    TDD Primera Prueba, creacion de objetos (Status: Fallo)

--- spec/math/fraction_spec.rb
+++ spec/math/fraction_spec.rb
@@ -1,7 +1,13 @@
-require "./racional.rb"
+require "./fraction.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
+    describe Fraction do
+        before :all do
+            A = Fraction.new(1,1)
+            B = Fraction.new(1,4)
+        end
+    end
 
 
 end
